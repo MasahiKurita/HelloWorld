1abb17047896768f145b1dcccd02a368ce131752
Masashi Kurita <marty@Masashis-MacBook-Pro.local>
2013-11-14 10:09:16 +0900

    Initial Commit

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Masashi Kurita on 2013/11/14.
+//  Copyright (c) 2013年 Masashi Kurita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Masashi Kurita on 2013/11/14.
+//  Copyright (c) 2013年 Masashi Kurita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
