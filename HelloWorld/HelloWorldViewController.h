5f0b34e57328f27d4f25a712bf5bfd22e379bcae
Masashi Kurita <marty@Masashis-MacBook-Pro.local>
2013-11-15 11:08:50 +0900

    Finalize app

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Masashi Kurita on 2013/11/14.
+//  Copyright (c) 2013年 Masashi Kurita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+- (IBAction) showMessage;
+
+@end
